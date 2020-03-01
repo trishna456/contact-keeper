4ea4a4f42dfdc1f34b2e506eb0772d8268dbcda7
trishna456 <trishna.patil13@gmail.com>
2020-03-02 01:07:15 +0530

    Express server setup with initial routes

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+// @route    POST api/users
+// @desc     Registers a user
+// @access   Public
+router.post('/', (req, res) => {
+  res.send('Registers a user');
+});
+
+module.exports = router;
